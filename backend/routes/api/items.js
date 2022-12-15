bac408be225a2e8c762d241b44ea1f5deca059fa
Sean Birmingham <sean.birmingham98@gmail.com>
2022-12-14 23:15:55 -0500

    refactored code

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -154,13 +154,15 @@ router.post('/', auth.required, function (req, res, next) {
         return res.sendStatus(401);
       }
 
-      const generatedImage = await openai.createImage({
-        prompt: req.body.item.title,
-        n: 1,
-        size: '256x256',
-      });
+      if (req.body.item.image === '') {
+        const generatedImage = await openai.createImage({
+          prompt: req.body.item.title,
+          n: 1,
+          size: '256x256',
+        });
 
-      req.body.item.image = generatedImage.data.data[0].url;
+        req.body.item.image = generatedImage.data.data[0].url;
+      }
 
       var item = new Item(req.body.item);
 
