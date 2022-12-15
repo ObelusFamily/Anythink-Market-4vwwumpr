31b8d8e52cdfd5bcfba3994797e5da2aa1bfd0e6
Sean Birmingham <sean.birmingham98@gmail.com>
2022-12-14 23:29:29 -0500

    test

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -8,11 +8,18 @@ const { sendEvent } = require('../../lib/event');
 const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
-  organization: 'org-PBQKGZGXfThCDUSEWz4r7zlp',
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
+let view = async function (req) {
+  const test = await openai.createImage({
+    prompt: req.body.item.title,
+    n: 2,
+    size: '256x256',
+  });
+  return test.data.data[0].url;
+};
 
 // Preload item objects on routes with ':item'
 router.param('item', function (req, res, next, slug) {
